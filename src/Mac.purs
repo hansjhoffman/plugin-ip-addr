c38b8b7151aa08490bce94bc037edc33f9098dc5
Hans Hoffman <hoffman.hans.j@gmail.com>
2023-06-17 17:00:38 -0600

    add upConvert

--- src/Mac.purs
+++ src/Mac.purs
@@ -2,6 +2,7 @@ module Mac
   ( Eui48(..)
   , Eui64(..)
   , MacAddr(..)
+  , upConvert_
   , parse_
   ) where
 
@@ -15,12 +16,10 @@ import Data.Show.Generic (genericShow)
 import Data.String as Data.String
 import Parsing (Parser)
 import Parsing as Parsing
-import Parsing.Combinators as Parsing.Combinators
-import Parsing.String.Basic as Parsing.String.Basic
 
 data MacAddr
-  = IPv4 Eui48 -- IPv4
-  | IPv6 Eui64 -- IPv6
+  = IPv4 Eui48
+  | IPv6 Eui64
 
 derive instance eqMacAddr :: Eq MacAddr
 
@@ -53,40 +52,49 @@ derive instance genericEui64 :: Generic Eui64 _
 instance showEui64 :: Show Eui64 where
   show = genericShow
 
+-- | INTERNAL
 pEui48 :: Parser String Eui48
 pEui48 = pSixGroupsByColon
   <|> pSixGroupsByHyphen
   <|> pThreeGroupsByDot
 
+-- | INTERNAL
 pSixGroupsByColon :: Parser String Eui48
 pSixGroupsByColon =
   pure $ SixGroupsByColon "FF:FF:FF:FF:FF:FF"
 
+-- | INTERNAL
 pSixGroupsByHyphen :: Parser String Eui48
 pSixGroupsByHyphen =
   pure $ SixGroupsByHyphen "FF-FF-FF-FF-FF-FF"
 
+-- | INTERNAL
 pThreeGroupsByDot :: Parser String Eui48
 pThreeGroupsByDot =
-  pure $ ThreeGroupsByDot "FFF.FFF.FFF.FFF"
+  pure $ ThreeGroupsByDot "FFFF.FFFF.FFFF"
 
+-- | INTERNAL
 pEui64 :: Parser String Eui64
 pEui64 = pEightGroupsByColon
   <|> pEightGroupsByHyphen
   <|> pFourGroupsByDot
 
+-- | INTERNAL
 pEightGroupsByColon :: Parser String Eui64
 pEightGroupsByColon =
   pure $ EightGroupsByColon "FF:FF:FF:FF:FF:FF:FF:FF"
 
+-- | INTERNAL
 pEightGroupsByHyphen :: Parser String Eui64
 pEightGroupsByHyphen =
   pure $ EightGroupsByColon "FF-FF-FF-FF-FF-FF-FF-FF"
 
+-- | INTERNAL
 pFourGroupsByDot :: Parser String Eui64
 pFourGroupsByDot =
   pure $ FourGroupsByDot "FFFF.FFFF.FFFF.FFFF"
 
+-- | INTERNAL
 parser :: Parser String MacAddr
 parser = (IPv4 <$> pEui48) <|> (IPv6 <$> pEui64)
 
@@ -101,4 +109,17 @@ prettyError err = msg <> " starting at position " <> show col
 parse_ :: String -> Either String MacAddr
 parse_ = lmap prettyError
   <<< flip Parsing.runParser parser
-  <<< Data.String.trim
+  <<< (Data.String.toUpper <<< Data.String.trim)
+
+-- | Convert ...
+upConvert_ :: Eui48 -> Eui64
+upConvert_ = case _ of
+  SixGroupsByColon a -> do
+    let parts = Data.String.splitAt 9 a
+    EightGroupsByColon (parts.before <> ":FF:FE" <> parts.after)
+  SixGroupsByHyphen b -> do
+    let parts = Data.String.splitAt 9 b
+    EightGroupsByHyphen (parts.before <> "-FF-FE" <> parts.after)
+  ThreeGroupsByDot c -> do
+    let parts = Data.String.splitAt 7 c
+    FourGroupsByDot (parts.before <> "FF.FE" <> parts.after)
